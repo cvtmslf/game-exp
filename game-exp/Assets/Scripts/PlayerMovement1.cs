6eb75643a581b70b8adc2eb70548bec0b4eef888
cvtmslf <volomoree2@mail.ru>
2024-04-22 02:03:20 +0300

    fixfixfix_commit
    dsdad

--- game-exp/Assets/Scripts/PlayerMovement1.cs
+++ game-exp/Assets/Scripts/PlayerMovement1.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Vector2 targetPos;
+    public float dashRange;
     public float speed;
-    public Vector2 direction;   
+    public Vector2 direction;
     private Vector2 directionHistory;
     private Animator animator;
+    private enum Facing { UP, DOWN, LEFT, RIGHT };
+    private Facing FacingDir = Facing.DOWN;
 
     private void Start()
     {
@@ -40,22 +44,50 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKey(KeyCode.W))
         {
             direction += Vector2.up;
+            FacingDir = Facing.UP;
         }
         if(Input.GetKey(KeyCode.A))
         {
             direction += Vector2.left;
+            FacingDir = Facing.LEFT;
         }
         if(Input.GetKey(KeyCode.S))
         {
             direction += Vector2.down;
+            FacingDir = Facing.DOWN;
         }
         if(Input.GetKey(KeyCode.D))
         {
             direction += Vector2.right;
+            FacingDir = Facing.RIGHT;
         }
         if (direction != Vector2.zero)
         {
             directionHistory = direction;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 currentPos = transform.position;
+            targetPos = Vector2.zero;
+
+            if (FacingDir == Facing.UP)
+            {
+                targetPos.y = 1;
+            }
+            else if (FacingDir == Facing.DOWN)
+            {
+                targetPos.y = -1;
+            }
+            else if (FacingDir == Facing.RIGHT)
+            {
+                targetPos.x = 1;
+            }
+            else if (FacingDir == Facing.LEFT)
+            {
+                targetPos.x = -1;
+            }
+            transform.Translate(targetPos * dashRange);
         }
 
     }
