50db7d5cca19477bbcf73ad668332a826a9c00e2
aganydanyda <kykyshka0118@gmail.com>
2024-04-22 08:44:06 +0300

    17_commit
    хелф бар и стамина бар. стамина бар пока не работает.

--- game-exp/Assets/Scripts/PlayerStats.cs
+++ game-exp/Assets/Scripts/PlayerStats.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public static PlayerStats playerStats;
 
     public GameObject player;
+    public Text healthText;
+    public Slider healthSlider;
 
     public float health;
     public float maxHealth;
-    
+
 
     private void Awake()
     {
@@ -30,20 +33,28 @@ public class PlayerStats : MonoBehaviour
     void Start()
     {
         health = maxHealth;
+        SetHealUI();
     }
 
     public void DealDamage(float damage)
     {
         health -= damage;
         CheckDeath();
+        SetHealUI();
     }
 
     public void HealCharacter(float heal)
     {
         health += heal;
         CheckOverHeal();
+        SetHealUI();
     }
 
+    private void SetHealUI()
+    {
+        healthSlider.value = CalculateHealthPercentage();
+        healthText.text = Mathf.Ceil(health).ToString() + " / " + Mathf.Ceil(maxHealth).ToString();
+    }
     public void CheckOverHeal()
     {
 
@@ -56,9 +67,14 @@ public class PlayerStats : MonoBehaviour
     {
         if (health <= 0)
         {
+            health = 0;
             Destroy(player);
         }
     }
 
-    
+    public float CalculateHealthPercentage()
+    {
+        return (health / maxHealth);
+    }
+
 }
